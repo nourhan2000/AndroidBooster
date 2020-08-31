9ce06f96aa41d31a0fd5ba775038792e724ac4dd
Omar2Mohamed2 <omar.helmy@outlook.com>
2020-08-31 12:14:00 +0200

    Repository completed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            //code for loading image from url
+        /* val image_url = ""
+        Picasso.get().load(image_url).into(image_view)
+         */
     }
 }
--- app/src/main/java/com/example/myapplication/network/modules/MovieResponse.kt
+++ app/src/main/java/com/example/myapplication/network/modules/MovieResponse.kt
@@ -1,5 +1,12 @@
 package com.example.myapplication.network.modules
 
-class MovieResponse {
+import com.google.gson.annotations.SerializedName
 
-}
+data class MovieResponse(
+    @SerializedName("results")
+    val MoviesList : List<MoviesDetails>
+)
+
+data class WeatherDetails(TODO
+//TODO 7oto hena el7agat eli gowa elresults(original_title,poster_path)
+)
