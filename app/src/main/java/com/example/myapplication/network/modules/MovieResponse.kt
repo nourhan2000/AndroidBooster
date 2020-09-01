9b4fd1ca0cc6871098427ca66bb34d98eeb333cc
Remon Emad <remonemad658@gmail.com>
2020-09-01 19:07:03 +0200

    data classes of MovieResponse are done

--- app/src/main/java/com/example/myapplication/network/modules/MovieResponse.kt
+++ app/src/main/java/com/example/myapplication/network/modules/MovieResponse.kt
@@ -3,9 +3,19 @@ package com.example.myapplication.network.modules
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("total_results")
+    val TotalResults: Int,
+    @SerializedName("total_pages")
+    val TotalPages: Int,
     @SerializedName("results")
     val MoviesList : List<MoviesDetails>
 )
 
-data class MoviesDetails( TODO( 7oto hena el7agat eli gowa elresults(original_title,poster_path))
+data class MoviesDetails(
+    @SerializedName("poster_path")
+    val PosterPath: Int,
+    @SerializedName("original_title")
+    val OriginalTitle: String,
 )
