a9a171b9d93140332c267c3eb0eb90c28a4a7828
Remon Emad <remonemad658@gmail.com>
2020-09-14 14:15:57 +0200

    add getMovieReviews fun in APIinterface.kt

--- app/src/main/java/com/example/myapplication/data/network/APIinterface.kt
+++ app/src/main/java/com/example/myapplication/data/network/APIinterface.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication.data.network
 import com.example.myapplication.data.modules.MovieResponse
+import com.example.myapplication.data.modules.MoviesReviews
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface APIinterface {
 
     @GET("movie/popular")
     fun getPopularMovie(@Query ("api_key") apiKey:String): Call<MovieResponse >
+    @GET("movie/{movie_id}/reviews")
+    fun getMovieReviews(@Query ("api_key") apiKey:String, @Path("movie_id") movieId:Long): Call<MoviesReviews>
+
 }
