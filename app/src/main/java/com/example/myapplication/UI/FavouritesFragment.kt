ff82177b921f0c295a5752fd5aca0eca15abb94d
Remon Emad <remonemad658@gmail.com>
2020-09-20 00:11:39 +0200

    add setOnClickListener in MovieAdapter.kt

--- app/src/main/java/com/example/myapplication/UI/FavouritesFragment.kt
+++ app/src/main/java/com/example/myapplication/UI/FavouritesFragment.kt
@@ -1,14 +1,32 @@
 package com.example.myapplication.UI
 
+import android.content.ContentValues.TAG
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest.newInstance
+import android.net.wifi.p2p.nsd.WifiP2pUpnpServiceRequest.newInstance
 import android.os.Bundle
+import android.service.controls.ControlsProviderService.TAG
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.Constraints.TAG
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.R
+import com.google.android.youtube.player.YouTubePlayerFragment.newInstance
+import com.google.android.youtube.player.YouTubePlayerSupportFragment.newInstance
 import kotlinx.android.synthetic.main.favourites_fragment.*
 import kotlinx.android.synthetic.main.top_movies_fragment.*
+
+import kotlinx.android.synthetic.main.popular_movies_fragment.*
+import org.xmlpull.v1.XmlPullParserFactory.newInstance
+import java.lang.reflect.Array.newInstance
+import javax.xml.datatype.DatatypeFactory.newInstance
+import javax.xml.parsers.DocumentBuilderFactory.newInstance
+import javax.xml.parsers.SAXParserFactory.newInstance
+import javax.xml.transform.TransformerFactory.newInstance
+import javax.xml.xpath.XPathFactory.newInstance
+
 
 
 class FavouritesFragment : Fragment() {
@@ -26,6 +44,7 @@ class FavouritesFragment : Fragment() {
         return view
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel.onError.observe(viewLifecycleOwner,{
@@ -40,6 +59,8 @@ class FavouritesFragment : Fragment() {
 
         mainViewModel.loadFavMovie()
     }
+
+
 
 
 }
